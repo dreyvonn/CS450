8da2f3f04d92c18889742bcd1d1da24d8fb73f42
dreyvonn <38699352+dreyvonn@users.noreply.github.com>
2018-09-30 14:49:49 -0600

    Delete HardCodedModel.py

--- HardCodedModel.py
+++
@@ -1,7 +0,0 @@
-class HardCodedModel:
-    def predict(self, data_test):
-        prediction = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-                      0, 0, 0, 0, 0]
-        return prediction
